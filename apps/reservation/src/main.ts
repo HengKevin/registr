f740afb1d957f8b6841fa50149d2ccdd7a00bf6e
Kev-world <hengkevin03@gmail.com>
2023-10-30 20:22:22 +0700

    validation and system logging

--- apps/reservation/src/main.ts
+++ apps/reservation/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ReservationModule } from './reservation.module';
+import { ValidationPipe } from '@nestjs/common';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(ReservationModule);
+  // If set to true validator will strip validated object of any properties that do not have any decorators.
+  // whitelisting request body
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useLogger(app.get(Logger));
   await app.listen(3000);
 }
 bootstrap();
