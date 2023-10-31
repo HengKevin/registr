d888ebd33ade7fe27917e50be7b6f7037a501ce7
Kev-world <hengkevin03@gmail.com>
2023-10-31 16:56:26 +0700

    user setup and passport strategy

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
-import { ReservationModule } from './reservation.module';
+import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
 import { Logger } from 'nestjs-pino';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReservationModule);
-  // If set to true validator will strip validated object of any properties that do not have any decorators.
-  // whitelisting request body
+  const app = await NestFactory.create(AuthModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(Logger));
   const configService = app.get(ConfigService);
--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
-import { ConfigurableModuleClass } from '@nestjs/common/cache/cache.module-definition';
-import { ConfigModule } from '../config/config.module';
-
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('MONGODB_URI'),
       }),
